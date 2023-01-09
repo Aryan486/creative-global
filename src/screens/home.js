70dc04475631d6fbb385736b5928b6783fc19938
Aryan486 <76522834+Aryan486@users.noreply.github.com>
2023-01-09 22:29:32 +0530

    New changes

--- src/screens/home.js
+++ src/screens/home.js
@@ -4,21 +4,25 @@ import ServicesImage from "../assets/images/section1a.png"
 import Work from "../assets/images/section1Work.png"
 import JoinUs from "../assets/images/section1Join.png"
 import ContactUs from "../assets/images/section1Contact.png"
-import Cipla from "../assets/images/Cipla.png"
-import DECATHLON from "../assets/images/DECATHLON.png"
+import Cipla from "../assets/images/cipla.png"
+import DECATHLON from "../assets/images/decathlon.png"
 import du from "../assets/images/du.png"
-import DUBAI from "../assets/images/DUBAI.png"
-import EXIDE from "../assets/images/EXIDE.png"
-import IndianOil from "../assets/images/IndianOil.png"
-import ITC from "../assets/images/ITC.png"
-import Jio from "../assets/images/Jio.png"
-import JCB from "../assets/images/JCB.png"
-import Mercedes from "../assets/images/Mercedes.png"
+import DUBAI from "../assets/images/dubai.png"
+import EXIDE from "../assets/images/exide.png"
+import IndianOil from "../assets/images/indianOil.png"
+import ITC from "../assets/images/itc.png"
+import Jio from "../assets/images/jio.png"
+import JCB from "../assets/images/jcb.png"
+import Mercedes from "../assets/images/mercedes.png"
 import SwatchaBharat from "../assets/images/SwatchaBharat.png"
-import Lenovo from "../assets/images/Lenovo.png"
-import WELSPUN from "../assets/images/WELSPUN.png"
-import SeedWorks from "../assets/images/SeedWorks.png"
+import Lenovo from "../assets/images/lenovo.png"
+import WELSPUN from "../assets/images/welspun.png"
+import SeedWorks from "../assets/images/seedWorks.png"
 import Rectangle from "../assets/images/section4Rectangle.png"
+import immersive from "../assets/images/immersiveTech.png"
+import mediaProd from "../assets/images/mediaProd.png"
+import serviceDesign from "../assets/images/serviceDesign.png"
+import smallMarket from "../assets/images/smallMarket.png"
 
 export default function Home() {
 
@@ -100,6 +104,57 @@ export default function Home() {
           <div className='section-4'>
             <img src={Rectangle} alt="Rectangle" className='section-4-rectangle' />
             
+          </div>
+          <div className='section-5'>
+            <h1 className='section-5-header'><span style={{color:"#2192FB"}}>Our Services</span> <br /><span style={{marginLeft:"10%"}}></span>& Capabilities</h1>
+            <div className='sub-section-1'>
+              <div className='left-1'>
+                <img src={immersive} alt="Rectangle" style={{height:"80%",width:"80%"}} /> 
+              </div>
+              <div className='right-1'>
+                <div style={{display:"flex",flexDirection:'column'}}>
+                  <h1 className='section-5-service-text'>Immersive Technology</h1>
+                  <h1 className='section-5-service-sub-text'>Taking Google Street View to record and reveal the state of the ocean</h1>
+                  <button className='section-5-button'>Learn More</button>
+                </div>
+              </div>
+            </div>
+            <div className='sub-section-1-temp'>
+              <div className='left-1'>
+                <div style={{display:"flex",flexDirection:'column'}}>
+                  <h1 className='section-5-service-text'>Media Production</h1>
+                  <h1 className='section-5-service-sub-text'>Our race to reveal the impact of climate change on the world's coral reef</h1>
+                  <button className='section-5-button'>Learn More</button>
+                </div>  
+              </div>
+              <div className='right-1'>
+                <img src={mediaProd} alt="Rectangle" style={{height:"80%",width:"80%"}} /> 
+              </div>
+            </div>
+            <div className='sub-section-1'>
+              <div className='left-1'>
+                <img src={serviceDesign} alt="Rectangle" style={{height:"80%",width:"80%"}} /> 
+              </div>
+              <div className='right-1'>
+                <div style={{display:"flex",flexDirection:'column'}}>
+                  <h1 className='section-5-service-text'>Design</h1>
+                  <h1 className='section-5-service-sub-text'>Taking Google Street View to record and reveal the state of the ocean</h1>
+                  <button className='section-5-button'>Learn More</button>
+                </div>
+              </div>
+            </div>
+            <div className='sub-section-1'>
+              <div className='left-1'>
+              <div style={{display:"flex",flexDirection:'column'}}>
+                  <h1 className='section-5-service-text'>Immersive Technology</h1>
+                  <h1 className='section-5-service-sub-text'>Taking Google Street View to record and reveal the state of the ocean</h1>
+                  <button className='section-5-button'>Learn More</button>
+                </div>
+              </div>
+              <div className='right-1'>
+                <img src={smallMarket} alt="Rectangle" style={{height:"80%",width:"80%"}} /> 
+              </div>
+            </div>
           </div>
       </div>
     </div>
